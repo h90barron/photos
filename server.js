7813daaf2eab57ac906f4bb0194a6389f2785bca
hb90 <h90barron@gmail.com>
2016-12-07 19:42:33 +0000

    Handle query string basics

--- server.js
+++ server.js
@@ -98,6 +98,7 @@
 
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
 function load_album_list(callback) {
   fs.readdir(
@@ -137,7 +138,8 @@ function load_album_list(callback) {
 }
 
 
-function load_album(album_name, callback) {
+
+function load_album(album_name, page, page_size, callback) {
   fs.readdir(
     "albums/" + album_name,
     function(err, files) {
@@ -153,10 +155,13 @@ function load_album(album_name, callback) {
     var only_files = [];
     var path = "albums/" + album_name + "/";
     
+    //called recursively 
     (function iter(index) {
       if (index == files.length) {
+        // page should be 0 in the query string for first set of results
+        var ps = only_files.splice(page * page_size, page_size);
         var obj = { short_name: album_name,
-                    photos: only_files };
+                    photos: ps };
         callback(null, obj);
         return;
       }
@@ -179,61 +184,35 @@ function load_album(album_name, callback) {
     });
 }
 
-// function load_album(album_name, callback) {
-//   //under the assumption that any directoy under albums is an album
-//   fs.readdir(
-//     "albums/" + album_name,
-//     function (err, files) {
-//       if (err) {
-//         if (err.code == "ENOENT") {
-//           callback(no_such_album());
-//         } else {
-//           callback(make_error("file error",
-//                               JSON.stringify(err)));
-//         }
-//         return
-//       }
-      
-//       var only_files = [];
-//       var path = "albums/" + album_name + "/";
-      
-//       (function iter(index) {
-//         if( index == files.length) {
-//           var obj = { short_name: album_name,
-//                       photos: only_files };
-//         }
-        
-//         fs.stat(
-//           path + files[index],
-//           function (err, stats) {
-//             if (err) {
-//               callback(make_error("file_error", JSON.stringify(err)));
-//               return;
-//             }
-//             if (stats.isFile()) {
-//               var obj = { filename: files[index],
-//                           desc: files[index] };
-//               only_files.push(obj);
-//             }
-//             iter(index + 1);
-//           });
-//       })(0);
-//     });
-// }
-
-
-
 function handle_incoming_request(req, res) {
   console.log("INCOMING REQUEST: " + req.method + " " + req.url);
-  if (req.url == '/albums.json') {
+  
+  req.parsed_url = url.parse(req.url, true);
+  var core_url = req.parsed_url.pathname;
+  
+  if (core_url == '/albums.json') {
     handle_list_albums(req, res);
-  } else if (req.url.substr(0, 7) == '/albums'
-            && req.url.substr(req.url.length - 5) == '.json') {
-              handle_get_album(req, res);
-  } else {
-    send_failure(res, 404, invalid_resource());
-  }
+    } else if (core_url.substr(0, 7) == '/albums' 
+              && core_url.substr(core_url.length - 5) == '.json') {
+                handle_get_album(req, res);
+    } else {
+      send_failure(res, 404, invalid_resource());
+    }
+          
 }
+
+
+// function handle_incoming_request(req, res) {
+//   console.log("INCOMING REQUEST: " + req.method + " " + req.url);
+//   if (req.url == '/albums.json') {
+//     handle_list_albums(req, res);
+//   } else if (req.url.substr(0, 7) == '/albums'
+//             && req.url.substr(req.url.length - 5) == '.json') {
+//               handle_get_album(req, res);
+//   } else {
+//     send_failure(res, 404, invalid_resource());
+//   }
+// }
 
 function handle_list_albums(req, res) {
   load_album_list(function(err, albums) {
@@ -247,11 +226,22 @@ function handle_list_albums(req, res) {
 }
 
 function handle_get_album(req, res) {
-  var album_name = req.url.substr(7, req.url.length - 12);
+  
+  var query = req.parsed_url.query;
+  var page_num = query.page ? query.page : 0;
+  var page_size = query.page_size ? query.page_size : 1000;
+  
+  if (isNaN(parseInt(page_num))) page_num = 0;
+  if (isNaN(parseInt(page_size))) page_size = 1000;
+  
+  var core_url = req.parsed_url.pathname;
+  var album_name = core_url.substr(7, core_url.length - 12);
   console.log(album_name);
   
   load_album(
     album_name,
+    page_num,
+    page_size,
     function (err, album_contents) {
       if (err && err.error == "no_such_album") {
         send_failure(res, 404, err);
